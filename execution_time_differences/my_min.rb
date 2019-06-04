d7b139bb9f7481d15e230c97470869076a447f20
ikesh pack <packappsforyou@gmail.com>
2019-06-04 13:18:32 -0400

    Defined my min

---
+++ execution_time_differences/my_min.rb
@@ -0,0 +1,16 @@
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+
+# Phase I
+# First, write a function that compares each element to every other element of the list. 
+# Return the element if all other elements in the array are larger.
+# What is the time complexity for this function?
+
+def my_min(arr)
+    arr.each do |num|
+        return num if arr.select { |other_num| other_num > num }
+    end
+end
+
+# O = n * n
+
+p my_min(list) # =>  -5
